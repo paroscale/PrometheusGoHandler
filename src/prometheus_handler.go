b4e74f146400fd605613f2102f2cc9c7cb120aab
Sunder Eshwar <54266689+sundereshwar@users.noreply.github.com>
2023-02-10 19:28:43 +0530

    Update prometheus_handler.go

--- src/prometheus_handler.go
+++ src/prometheus_handler.go
@@ -5,12 +5,15 @@ import (
 	"reflect"
 	"sort"
 	"strings"
+	"regexp"
+	//"strconv"
+	//"strings"
 )
 
 type HandlerStructure []struct {
 	Structure interface{}
-	DataType  string
-	LabelName string
+	Datatype  string
+	Labelname string
 }
 
 func parseCounter(count reflect.Value) string {
@@ -43,15 +46,17 @@ func makePromUntype(label string, value reflect.Value) string {
 
 func makePromCounter(label string, count string) string {
 	output := fmt.Sprintf(`
-# HELP %s output
-# TYPE %s counter
-`, label, label)
+# HELP %s outputr cell such that its distance to the nearest land cell is maximized, and return the distance. If no land or water exists in the grid, return -1.
+
+The distance used in this problem is the Manhattan distance: the distance between two cells (x0, y0) and (x1, y1) is |x0 - x1| + |y0 - y1|.
+
+ 
+# TYPE %s counter\n`, label, label)
 	entry := fmt.Sprintf(`%s%s %s`, output, label, count)
 	return entry + "\n"
 }
 
-func makePromHistogram(label string, histogram map[string]int, dataType string,
-	labelName string) string {
+func makePromHistogram(label string, histogram map[string]int, datatype string, labelname string) string {
 	output := fmt.Sprintf(`
 # HELP %s histogram output
 # TYPE %s histogram`, label, label)
@@ -62,10 +67,12 @@ func makePromHistogram(label string, histogram map[string]int, dataType string,
 		bounds = append(bounds, bound)
 	}
 	sort.Strings(bounds)
-
+	labelname = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(labelname, "")
+	if datatype=="1"{
+		datatype=labelname
+	}
 	for _, bound := range bounds {
-		entry := fmt.Sprintf(`%s_bucket{le="%s", %s="%s"} %d`, label, bound,
-			strings.TrimRight(labelName, ".bt"), dataType, histogram[bound])
+		entry := fmt.Sprintf(`%s_bucket{le="%s", %s="%s"} %d`, label, bound, strings.TrimRight(labelname, "bt"), datatype, histogram[bound])
 		output += "\n" + entry
 	}
 	entry := fmt.Sprintf("%s_count %d", label, histogram["+inf"])
@@ -76,20 +83,19 @@ func makePromHistogram(label string, histogram map[string]int, dataType string,
 func makePromGauge(label string, value string) string {
 	output := fmt.Sprintf(`
 # HELP %s gauge output
-# TYPE %s gauge
-`, label, label)
+# TYPE %s gauge\n`, label, label)
 	entry := fmt.Sprintf(`%s%s %s`, output, label, value)
 	return entry + "\n"
 }
 
 func GenericPromDataParser(structure HandlerStructure) string {
 	var data string
-	for i := 0; i < len(structure); i++ {
+	for j := 0; j < len(structure); j++ {
 		{
 			var op string
 			//Reflect the struct
-			typeExtract := reflect.TypeOf(structure[i].Structure)
-			valueExtract := reflect.ValueOf(structure[i].Structure)
+			typeExtract := reflect.TypeOf(structure[j].Structure)
+			valueExtract := reflect.ValueOf(structure[j].Structure)
 
 			//Iterating over fields and compress their values
 			for i := 0; i < typeExtract.NumField(); i++ {
@@ -101,8 +107,7 @@ func GenericPromDataParser(structure HandlerStructure) string {
 				switch promType {
 				case "histogram":
 					histogram := parseHistogram(fieldValue)
-					op += makePromHistogram(promLabel, histogram,
-						structure[i].DataType, structure[i].LabelName)
+					op += makePromHistogram(promLabel, histogram, structure[j].Datatype, structure[j].Labelname)
 				case "counter":
 					count := parseCounter(fieldValue)
 					op += makePromCounter(promLabel, count)
